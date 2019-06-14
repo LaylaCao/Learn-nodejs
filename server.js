7552472c32d64cdf12ec85be75ede4e9e52c4e89
caojiali <caojiali1995@gmail.com>
2019-06-14 14:18:40 +0800

    Update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http');
 
+// 调用 http 模块提供的函数： createServer 。
+// 这个函数会返回 一个对象，这个对象有一个叫做 listen 的方法，这个方法有一个数值参数， 指定这个 HTTP 服务器监听的端口号。
+
 http.createServer(function(request, response) {
 
     // 发送HTTP头部
